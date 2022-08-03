a183e3f7ffdfa7af8137f8b9b1d69ab344229003
Ahmed Magdy <83177544+3AMEER@users.noreply.github.com>
2022-08-03 15:08:09 +0200

    Add files via upload

---
+++ style.js
@@ -0,0 +1,101 @@
+const fadeOut = () => {
+    const loderwrapper = document.querySelector('.wrapper');
+    loderwrapper.classList.add('fade');
+
+}
+
+window.addEventListener('load', fadeOut)
+
+
+
+
+// Strat Boxs
+const boxes = document.querySelectorAll(".box");
+
+const checkBoxes = () => {
+    const triggerBottom = (window.innerHeight / 4) * 3;
+    boxes.forEach((box) =>{
+        const boxTop = box.getBoundingClientRect().top;
+        if (boxTop < triggerBottom) box.classList.add("show");
+        else box.classList.remove("show")
+    });
+};
+
+window.addEventListener("scroll", checkBoxes);
+checkBoxes()
+
+
+
+// Start JS Swipper
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 10,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//       },
+//     breakpoints: {
+//       640: {
+//         slidesPerView: 2,
+//         spaceBetween: 20,
+//       },
+//       768: {
+//         slidesPerView: 4,
+//         spaceBetween: 40,
+//       },
+//       1024: {
+//         slidesPerView: 6,
+//         spaceBetween: 50,
+//       },
+//     },
+// });
+
+var mybotton = document.getElementById('goup');
+
+window.onscroll = function () {
+
+    // console.log(scrollY)
+    'user strict';
+
+    if (window.pageYOffset >= 800){
+        mybotton.style.display = "block";        
+    }
+    else{
+        mybotton.style.display = "none";        
+    }
+
+};
+
+mybotton.onclick = function () {
+    'user strict';
+
+    window.scrollTo({
+        top: 0,       
+        behavior: "smooth",
+    });
+}
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
